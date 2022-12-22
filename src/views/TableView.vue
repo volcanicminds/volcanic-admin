54468f30941e16360a8e2a5a805a7780ed2134ab
Davide Dispenza <davide.dispenza@epicura.it>
2022-12-22 16:49:12 +0100

    Fix multiple calls on change route

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -85,14 +85,14 @@ export default defineComponent({
 
 	watch: {
 		$route(to, from) {
-			this.params = {
-				sorting: {},
-				filters: [],
-				pagination: {
-					pageIndex: START_PAGE_INDEX,
-					pageSize: DEFAULT_PAGE_SIZE
-				}
-			}
+			// this.params = {
+			// 	sorting: {},
+			// 	filters: [],
+			// 	pagination: {
+			// 		pageIndex: START_PAGE_INDEX,
+			// 		pageSize: DEFAULT_PAGE_SIZE
+			// 	}
+			// }
 			this.initialize()
 		}
 	},
@@ -137,172 +137,205 @@ export default defineComponent({
 			}
 
 			this.model = modelConfiguration?.columns
-
 			this.source = `/${menuItem.source}`
 
-			const tableStoreConfiguration = tableStore.tables.configuration[menuItem.name || indexSource]
+			let paramsHaveChange = false
 
+			const tableStoreConfiguration = tableStore.tables.configuration[menuItem.name || indexSource]
 			if (tableStoreConfiguration) {
+				paramsHaveChange = !_.isEqual(this.params, tableStoreConfiguration)
 				this.params = tableStoreConfiguration
 			} else {
-				const getModelParameters = prepareModelParamenters(this.model)
-				const urlParams = this.$route.query
-				const urlPagination = {} as TablePaginationParams
-				const urlFilters = [] as TableFiltersParams
-				const urlSorting = {} as TableSortingParams
+				const updatedParams = this.getParams({ menu: menuItem, configuration: modelConfiguration })
 
-				Object.keys(urlParams).forEach((k) => {
-					if (k.startsWith(FILTER_URL_PREFIX)) {
-						const normalizedK = k.replace(FILTER_URL_PREFIX, '')
-						const [key, operator = FILTER_OPERATORS.EQ, index] = normalizedK.split(':')
-						urlFilters.push({
-							key,
-							operator,
-							value: urlParams[k]
-						})
-					}
-					if (k.startsWith(SORT_URL_PREFIX)) {
-						const normalizedK = k.replace(SORT_URL_PREFIX, '')
-						urlSorting[normalizedK] = urlParams[k] === 'no' ? '' : urlParams[k]
-					}
-					if (k.startsWith(PAGINATION_URL_PREFIX)) {
-						const normalizedK = k.replace(PAGINATION_URL_PREFIX, '')
-						urlPagination[normalizedK] = Number(urlParams[k])
-					}
-				})
-
-				const { pagination: menuPagination = {}, sorting: menuSorting = {}, filters: menuFilters = [] } = menuItem
-				const modelPagination = modelConfiguration?.table?.pagination || {}
-				this.params = {
-					pagination: {
-						...{ pageSize: this.params.pagination?.pageSize, pageIndex: this.params.pagination?.pageIndex },
-						...modelPagination,
-						...menuPagination,
-						...urlPagination
-					},
-					filters: deleteFilterDuplicates([
-						...(getModelParameters('filters') as TableFiltersParams),
-						...menuFilters,
-						...urlFilters
-					]),
-					sorting: { ...(getModelParameters('sorting') as TableSortingParams), ...menuSorting, ...urlSorting }
-				}
-
+				paramsHaveChange = !_.isEqual(this.params, updatedParams)
+				this.params = updatedParams
 				this.updateStoreConfiguration()
 			}
 
-			this.updateRouteQuery()
+			if (paramsHaveChange) {
+				this.updateRouteQuery()
 
-			try {
-				await this.find()
-			} catch (e) {
-				console.error(e)
+				try {
+					await this.find()
+				} catch (e) {
+					console.error(e)
+				}
+
+				this.updateTable({ menu: menuItem, configuration: modelConfiguration, indexSource })
 			}
 
+			this.loader.close()
+		},
+		updateTable: function ({
+			menu,
+			indexSource,
+			configuration
+		}: {
+			menu: MenuItem
+			indexSource: string
+			configuration: ConfigSourceModel
+		}) {
+			const tableStore = useTablesStore()
 			const isConfigured = this.model != null
 			const hasTableData = this.table.length > 0
-			const storedColumnDefinitions = tableStore.tables.columnDefinitions[menuItem.name || indexSource]
+			const storedColumnDefinitions = tableStore.tables.columnDefinitions[menu.name || indexSource]
 
 			if (storedColumnDefinitions) {
 				this.columnDefs = storedColumnDefinitions
 			} else if (isConfigured || hasTableData) {
-				const sourceOfTableData = isConfigured ? this.model : this.table
-				this.columnDefs = Object.keys(sourceOfTableData).map((key, index) => {
-					const rowModel = this.model[key] || {}
+				this.setColumnDefinition({ isConfigured, configuration, indexSource })
+			}
+		},
+		setColumnDefinition: function ({
+			isConfigured,
+			configuration,
+			indexSource
+		}: {
+			isConfigured: boolean
+			configuration: ConfigSourceModel
+			indexSource: string
+		}) {
+			const sourceOfTableData = isConfigured ? this.model : this.table
+			this.columnDefs = Object.keys(sourceOfTableData).map((key, index) => {
+				const rowModel = this.model[key] || {}
 
-					const sortBy = this.params.sorting ? this.params.sorting[extendSingleSortKey(key, rowModel)] : undefined
+				const sortBy = this.params.sorting ? this.params.sorting[extendSingleSortKey(key, rowModel)] : undefined
 
-					return {
-						field: key,
-						title: key.toUpperCase(),
-						key,
-						width: rowModel?.table?.width,
-						sortBy,
-						//align left hides the filter button
-						//align: typeof sourceOfTableData[key] === 'number' ? 'right' : 'left',
-						renderBodyCell: ({ row, column, rowIndex }: { row: any; column: any; rowIndex: number }, h: any) => {
-							const value = row[column.field]
-							let normalizedValue = value
-							const cellFormat = rowModel?.table?.cell?.format
-							const formatType = typeof cellFormat
+				return {
+					field: key,
+					title: key.toUpperCase(),
+					key,
+					width: rowModel?.table?.width,
+					sortBy,
+					//align left hides the filter button
+					//align: typeof sourceOfTableData[key] === 'number' ? 'right' : 'left',
+					renderBodyCell: ({ row, column, rowIndex }: { row: any; column: any; rowIndex: number }, h: any) => {
+						const value = row[column.field]
+						let normalizedValue = value
+						const cellFormat = rowModel?.table?.cell?.format
+						const formatType = typeof cellFormat
 
-							if (typeof value === 'boolean') {
-								normalizedValue = value === true ? 'Sì' : 'No'
-							} else if (formatType === 'string') {
-								normalizedValue = dayjs(value).format(cellFormat)
-							} else if (formatType === 'function') {
-								normalizedValue = cellFormat(value)
-							}
+						if (typeof value === 'boolean') {
+							normalizedValue = value === true ? 'Sì' : 'No'
+						} else if (formatType === 'string') {
+							normalizedValue = dayjs(value).format(cellFormat)
+						} else if (formatType === 'function') {
+							normalizedValue = cellFormat(value)
+						}
 
-							const isLink = rowModel?.table?.cell?.isLink
+						const isLink = rowModel?.table?.cell?.isLink
 
-							if (isLink) {
-								return (
-									<router-link
-										title={`${this.model[column.field]?.input.label}`}
-										to={`${this.model[column.field]?.input.source}/${value?.id}`}
-									>
-										<a onClick={(e) => e.stopPropagation()}>{normalizedValue}</a>
-									</router-link>
-								)
-							}
+						if (isLink) {
+							return (
+								<router-link
+									title={`${this.model[column.field]?.input.label}`}
+									to={`${this.model[column.field]?.input.source}/${value?.id}`}
+								>
+									<a onClick={(e) => e.stopPropagation()}>{normalizedValue}</a>
+								</router-link>
+							)
+						}
 
-							return <span>{normalizedValue}</span>
-						},
-						filterCustom: {
-							defaultVisible: false,
-							// eslint-disable-next-line @typescript-eslint/no-unused-vars
-							render: ({ showFn, closeFn }: { showFn: () => void; closeFn: () => void }, h: any) => {
-								const filterModel = rowModel?.table?.filtering || ({} as TableFiltering)
-								const paramsFilters = this.params?.filters || []
-								const filterDefaultValues = paramsFilters.filter((f) => f.key === key).map((f) => f.value)
-								return filterModel?.enabled !== false ? (
-									<TableFilter
-										inputType={rowModel?.type || 'text'}
-										name={key}
-										subField={filterModel?.subField}
-										operator={filterModel.operator || 'eq'}
-										values={filterDefaultValues || []}
-										addFilter={this.addFilter}
-										searchCancel={() => this.searchCancel(closeFn)}
-										searchConfirm={() => this.searchConfirm(closeFn)}
-									/>
-								) : null
-							}
+						return <span>{normalizedValue}</span>
+					},
+					filterCustom: {
+						defaultVisible: false,
+						// eslint-disable-next-line @typescript-eslint/no-unused-vars
+						render: ({ showFn, closeFn }: { showFn: () => void; closeFn: () => void }, h: any) => {
+							const filterModel = rowModel?.table?.filtering || ({} as TableFiltering)
+							const paramsFilters = this.params?.filters || []
+							const filterDefaultValues = paramsFilters.filter((f) => f.key === key).map((f) => f.value)
+							return filterModel?.enabled !== false ? (
+								<TableFilter
+									inputType={rowModel?.type || 'text'}
+									name={key}
+									subField={filterModel?.subField}
+									operator={filterModel.operator || 'eq'}
+									values={filterDefaultValues || []}
+									addFilter={this.addFilter}
+									searchCancel={() => this.searchCancel(closeFn)}
+									searchConfirm={() => this.searchConfirm(closeFn)}
+								/>
+							) : null
 						}
 					}
-				})
-
-				if (modelConfiguration?.table?.rowMenu) {
-					const menu = (modelConfiguration?.table?.rowMenu || []) as TableOptionsMenuItems
-
-					//Setting the standard operation on case of delete
-					menu.forEach((m, index) => {
-						if (m.delete) {
-							menu[index].operation = this.deleteRow
-						}
-					})
-
-					this.columnDefs.push({
-						field: '',
-						title: '',
-						key: 'row-menu',
-						renderBodyCell: ({ row, column, rowIndex }: { row: any; column: any; rowIndex: number }, h: any) => {
-							return <TableOptionsMenu row={row} column={column} rowMenu={menu} />
-						}
-					})
 				}
+			})
 
-				Object.keys(sourceOfTableData).forEach((key) => {
-					if (this.model[key]?.table?.visible === false) {
-						this.columnHiddenOption.defaultHiddenColumnKeys.push(key)
+			if (configuration?.table?.rowMenu) {
+				const menu = (configuration?.table?.rowMenu || []) as TableOptionsMenuItems
+
+				//Setting the standard operation on case of delete
+				menu.forEach((m, index) => {
+					if (m.delete) {
+						menu[index].operation = this.deleteRow
 					}
 				})
 
-				tableStore.setColumnDefinition(indexSource, this.columnDefs)
+				this.columnDefs.push({
+					field: '',
+					title: '',
+					key: 'row-menu',
+					renderBodyCell: ({ row, column, rowIndex }: { row: any; column: any; rowIndex: number }, h: any) => {
+						return <TableOptionsMenu row={row} column={column} rowMenu={menu} />
+					}
+				})
 			}
 
-			this.loader.close()
+			Object.keys(sourceOfTableData).forEach((key) => {
+				if (this.model[key]?.table?.visible === false) {
+					this.columnHiddenOption.defaultHiddenColumnKeys.push(key)
+				}
+			})
+
+			const tableStore = useTablesStore()
+			tableStore.setColumnDefinition(indexSource, this.columnDefs)
+		},
+		getParams: function ({ menu, configuration }: { menu: MenuItem; configuration: ConfigSourceModel }) {
+			const getModelParameters = prepareModelParamenters(this.model)
+			const urlParams = this.$route.query
+			const urlPagination = {} as TablePaginationParams
+			const urlFilters = [] as TableFiltersParams
+			const urlSorting = {} as TableSortingParams
+
+			Object.keys(urlParams).forEach((k) => {
+				if (k.startsWith(FILTER_URL_PREFIX)) {
+					const normalizedK = k.replace(FILTER_URL_PREFIX, '')
+					const [key, operator = FILTER_OPERATORS.EQ, index] = normalizedK.split(':')
+					urlFilters.push({
+						key,
+						operator,
+						value: urlParams[k]
+					})
+				}
+				if (k.startsWith(SORT_URL_PREFIX)) {
+					const normalizedK = k.replace(SORT_URL_PREFIX, '')
+					urlSorting[normalizedK] = urlParams[k] === 'no' ? '' : urlParams[k]
+				}
+				if (k.startsWith(PAGINATION_URL_PREFIX)) {
+					const normalizedK = k.replace(PAGINATION_URL_PREFIX, '')
+					urlPagination[normalizedK] = Number(urlParams[k])
+				}
+			})
+
+			const { pagination: menuPagination = {}, sorting: menuSorting = {}, filters: menuFilters = [] } = menu
+			const modelPagination = configuration?.table?.pagination || {}
+
+			return {
+				pagination: {
+					...{ pageSize: this.params.pagination?.pageSize, pageIndex: this.params.pagination?.pageIndex },
+					...modelPagination,
+					...menuPagination,
+					...urlPagination
+				},
+				filters: deleteFilterDuplicates([
+					...(getModelParameters('filters') as TableFiltersParams),
+					...menuFilters,
+					...urlFilters
+				]),
+				sorting: { ...(getModelParameters('sorting') as TableSortingParams), ...menuSorting, ...urlSorting }
+			}
 		},
 		getDetailParams: function (row: any) {
 			return {
@@ -328,47 +361,37 @@ export default defineComponent({
 			this.table = tableData as TableData
 			this.tableTotalLength = tableCount as number
 		},
-		updateRouteQuery: async function () {
-			const [pagination, sorting, filters] = await Promise.all([
-				(() => {
-					return {
-						[`${PAGINATION_URL_PREFIX}pageIndex`]: String(this.params.pagination?.pageIndex),
-						[`${PAGINATION_URL_PREFIX}pageSize`]: String(this.params.pagination?.pageSize)
-					}
-				})(),
-				(() => {
-					const sorting = {} as TableSortingParams
-					Object.keys(this.params.sorting).forEach((k) => {
-						if (this.params.sorting[k] === '') {
-							sorting[`${SORT_URL_PREFIX}${k}`] = 'no'
-						}
-						if (this.params.sorting[k]) {
-							sorting[`${SORT_URL_PREFIX}${k}`] = String(this.params.sorting[k])
-						}
-					})
+		updateRouteQuery: function () {
+			const pagination = {
+				[`${PAGINATION_URL_PREFIX}pageIndex`]: String(this.params.pagination?.pageIndex),
+				[`${PAGINATION_URL_PREFIX}pageSize`]: String(this.params.pagination?.pageSize)
+			}
 
-					return sorting
-				})(),
-				(() => {
-					const filters = {} as { [key: string]: ApiGenericValue }
-					this.params.filters?.forEach((filter, index) => {
-						if (filter) {
-							filters[`${FILTER_URL_PREFIX}${filter.key}:${filter.operator}:${index}`] = String(filter.value)
-						}
-					})
+			const sorting = {} as TableSortingParams
+			Object.keys(this.params.sorting).forEach((k) => {
+				if (this.params.sorting[k] === '') {
+					sorting[`${SORT_URL_PREFIX}${k}`] = 'no'
+				}
+				if (this.params.sorting[k]) {
+					sorting[`${SORT_URL_PREFIX}${k}`] = String(this.params.sorting[k])
+				}
+			})
 
-					return filters
-				})()
-			])
+			const filters = {} as { [key: string]: ApiGenericValue }
+			this.params.filters?.forEach((filter, index) => {
+				if (filter) {
+					filters[`${FILTER_URL_PREFIX}${filter.key}:${filter.operator}:${index}`] = String(filter.value)
+				}
+			})
 
 			const query = {
 				...pagination,
 				...sorting,
 				...filters
 			}
-
 			if (!_.isEqual(this.$route.query, query)) {
 				this.$router.replace({
+					...this.$router.currentRoute,
 					query
 				})
 			}
@@ -437,7 +460,7 @@ export default defineComponent({
 			loader.show()
 
 			const filters = Array.from(this.params.filters || [])
-			this.params.filters?.forEach((filter, index) => {
+			this.params.filters?.forEach((filter: any, index: number) => {
 				if (!filter.value) {
 					delete filters[index]
 				}
